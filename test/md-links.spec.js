e8484ed6e8c64d328fb946588907798c7eb6f75e
Evelyn V Pírez <evepirez@gmail.com>
2023-10-17 13:47:31 -0300

    Test función mdlinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,11 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../lib/mdLinks');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('resolve promise', () => {
+    const filePath = './examples/readme.md';
+    console.log('filePath');
+    expect(filePath).toEqual('./examples/readme.md');
   });
 
 });
