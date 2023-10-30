ecd146bc049cd5f51a22aa349f7fbe8c7ef2ff75
Evelyn V Pírez <evepirez@gmail.com>
2023-10-30 13:21:43 -0300

    comentarios test y función mdLinks

--- test/extractLinks.spec.js
+++ test/extractLinks.spec.js
@@ -1,11 +1,14 @@
 const extractLinks = require('../lib/extractLinks.js');
 
 describe('extractLinks', () => {
+// sta prueba verifica si la función extractLinks extrae correctamente los enlaces del contenido de Markdown proporcionado.
   it('should extract links from the markdown content', () => {
+  // variable filePath que indica la ruta del archivo Markdown
     const markdownContent = `
       This is a [link](https://www.example.com) to an example website.
       Here is another [link](https://www.google.com).`;
-
+  // función extractLinks con el contenido de Markdown 
+  // y la ruta del archivo como argumentos y se espera que devuelva un array de objetos de enlace específicos
     const filePath = 'path/to/example.md';
     const links = extractLinks(markdownContent, filePath);
 
--- test/validateLinks.spec.js
+++ test/validateLinks.spec.js
@@ -1,16 +1,17 @@
 const axios = require('axios');
 const validateLinks = require('../lib/validateLinks');
-
+// la función jest.mock('axios') para simular el módulo axios
 jest.mock('axios');
 
 describe('validateLinksFunc', () => {
-
+// 'should handle undefined links'. 
+// Esta prueba verifica si la función validateLinks maneja correctamente los enlaces no definidos.
   it('should handle undefined links', async () => {
-
+// Se define una lista de enlaces donde uno de los objetos de enlace tiene href como undefined
     const links = [{ href: undefined }];
   
     const result = await validateLinks(links);
-  
+// se espera que el primer objeto de resultado tenga una propiedad status que sea igual a la cadena 'undefined'  
     expect(result[0].status).toBe('undefined');
   
   });
