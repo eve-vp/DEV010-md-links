3f1954239ba238a70cce113e0143ca469b33596e
Evelyn V Pírez <evepirez@gmail.com>
2023-10-30 14:00:05 -0300

    validateAndRequire

--- test/validateAndRequire.spec.js
+++ test/validateAndRequire.spec.js
@@ -1,14 +1,14 @@
-const isAbsolutePath = require('../lib/isAbsolutePath.js'); 
+const validateAndRequire = require('../validateAndRequire.js');
 
-describe('isAbsolutePath', () => {
+describe('validateAndRequire', () => {
   it('should return a promise that resolves with the file path if it exists', () => {
-    return isAbsolutePath('path/to/existing/file.md').then((result) => {
+    return validateAndRequire('path/to/existing/file.md').then((result) => {
       expect(result).toEqual('path/to/existing/file.md');
     });
   });
 
   it('should return a promise that rejects with an error message if the file path does not exist', () => {
-    return isAbsolutePath('path/to/nonexistent/file.md').catch((error) => {
+    return validateAndRequire('path/to/nonexistent/file.md').catch((error) => {
       expect(error).toEqual("The route 'path/to/nonexistent/file.md' was not found");
     });
   });
